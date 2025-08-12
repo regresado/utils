47d3c9fc3a59d983bb28c39b601af20eafd569b2
Josh Pinto <joshpinto6@outlook.com>
2025-08-12 16:25:54 -0400

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🔧 Regreso Utils
+
+[![npm](https://img.shields.io/badge/npm-CB3837?logo=npm&logoColor=fff)](https://www.npmjs.com/package/@regreso/utils)
+![NodeJS](https://img.shields.io/badge/Node.js-6DA55F?logo=node.js&logoColor=white)
+![TypeScript](https://img.shields.io/badge/TypeScript-3178C6?logo=typescript&logoColor=fff)
+
+Some neat little utilities relied upon by Regreso clients. Right now, these can be used to:
+
+- Get metadata info about websites (e.g. prefilling destination details automatically)
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { parse } from "node-html-parser";
+
+// TODO: Implement image utils
+
+type WebDetailsResult = {
+  url: string | undefined;
+  title: (string | undefined)[];
+  description: (string | undefined)[];
+  error?: string;
+};
+
+export async function getWebDetails(url: string): Promise<WebDetailsResult> {
+  const htmlResponse = await fetch(
+    url.startsWith("http") ? url : "https://" + url,
+    { signal: AbortSignal.timeout(1200) },
+  )
+    .then((res) => res.text())
+    .catch((_err) => {
+      return "Failed to fetch URL";
+    });
+
+  if (htmlResponse === "Failed to fetch URL") {
+    return {
+      error: htmlResponse,
+      url: url.startsWith("http") ? url : "https://" + url,
+      title: ["New Destination"],
+      description: [""],
+    };
+  }
+
+  const doc = parse(htmlResponse);
+  const title = [
+    doc.querySelector("title")?.text,
+    doc.querySelector('meta[property="og:title"]')?.getAttribute("content"),
+    doc.querySelector('meta[name="twitter:title"]')?.getAttribute("content"),
+  ];
+  const description = [
+    doc.querySelector('meta[name="description"]')?.getAttribute("content"),
+    doc
+      .querySelector('meta[property="og:description"]')
+      ?.getAttribute("content"),
+    doc
+      .querySelector('meta[name="twitter:description"]')
+      ?.getAttribute("content"),
+  ];
+
+  return {
+    url: url.startsWith("http") ? url : "https://" + url,
+    title,
+    description,
+  };
+}
